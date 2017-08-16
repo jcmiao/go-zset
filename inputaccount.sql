632144254350c664129e5b7a5b2ae8c57297eb84
miaojianchao@126.com <miaojianchao@126.com>
2017-08-16 17:24:34 +0800

    account

--- inputaccount.sql
+++ inputaccount.sql
@@ -1,23 +1,367 @@
 -- 账号表
-insert into gm_account(id, gm_user_name, gm_password) values(3, "test01", "e10adc3949ba59abbe56e057f20f883e");
+insert into gm_account(id, gm_user_name, gm_password) values(4, "kefu1", "1f9885b2e4cc9b8928d26673ae71dd52");
 
 -- 用户表
-insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(3, "test01", "test01", 1, 10019);
+insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(4, "kefu1", "kefu1", 1, 10019);
 
 
 
 -- 插入权限
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(51, 3, 1);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(52, 3, 2);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(53, 3, 11);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(54, 3, 13);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(55, 3, 14);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(56, 3, 16);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(57, 3, 17);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(58, 3, 18);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(60, 3, 19);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(61, 3, 20);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(62, 3, 22);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(63, 3, 23);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(64, 3, 25);
-insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(65, 3, 26);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(66, 4, 1);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(67, 4, 2);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(68, 4, 11);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(69, 4, 13);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(70, 4, 14);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(71, 4, 16);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(72, 4, 17);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(73, 4, 18);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(74, 4, 19);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(75, 4, 20);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(76, 4, 22);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(77, 4, 23);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(78, 4, 25);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(79, 4, 26);
+
+
+
+-- 账号表
+insert into gm_account(id, gm_user_name, gm_password) values(5, "kefu2", "f5182e62463d0b0c81504212935a7167");
+
+-- 用户表
+insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(5, "kefu2", "kefu2", 1, 10019);
+
+
+
+-- 插入权限
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(80, 5, 1);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(81, 5, 2);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(82, 5, 11);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(83, 5, 13);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(84, 5, 14);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(85, 5, 16);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(86, 5, 17);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(87, 5, 18);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(88, 5, 19);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(89, 5, 20);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(90, 5, 22);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(91, 5, 23);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(92, 5, 25);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(93, 5, 26);
+
+
+
+
+
+
+
+
+
+-- 账号表
+insert into gm_account(id, gm_user_name, gm_password) values(6, "kefu3", "96d2bd50f1ceb527d3d447d64d3f169c");
+
+-- 用户表
+insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(6, "kefu3", "kefu3", 1, 10019);
+
+
+
+-- 插入权限
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(94, 6, 1);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(95, 6, 2);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(96, 6, 11);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(97, 6, 13);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(98, 6, 14);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(99, 6, 16);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(100, 6, 17);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(101, 6, 18);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(102, 6, 19);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(103, 6, 20);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(104, 6, 22);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(105, 6, 23);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(106, 6, 25);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(107, 6, 26);
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 账号表
+insert into gm_account(id, gm_user_name, gm_password) values(7, "kefu4", "6bd14ea9d083d8be10ac35a2748a8443");
+
+-- 用户表
+insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(7, "kefu4", "kefu4", 1, 10019);
+
+
+
+-- 插入权限
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(108, 7, 1);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(109, 7, 2);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(110, 7, 11);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(111, 7, 13);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(112, 7, 14);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(113, 7, 16);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(114, 7, 17);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(115, 7, 18);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(116, 7, 19);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(117, 7, 20);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(118, 7, 22);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(119, 7, 23);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(120, 7, 25);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(121, 7, 26);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 账号表
+insert into gm_account(id, gm_user_name, gm_password) values(8, "dakehu1", "55a528bc7dcd20aacbe6a3468341fa26");
+
+-- 用户表
+insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(8, "dakehu1", "dakehu1", 1, 10019);
+
+
+
+-- 插入权限
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(122, 8, 1);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(123, 8, 2);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(124, 8, 11);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(125, 8, 13);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(126, 8, 14);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(127, 8, 16);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(128, 8, 17);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(129, 8, 18);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(130, 8, 19);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(131, 8, 20);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(132, 8, 22);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(133, 8, 23);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(134, 8, 25);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(135, 8, 26);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 账号表
+insert into gm_account(id, gm_user_name, gm_password) values(9, "dakehu2", "939c2378188f8d539412179e2f79ae89");
+
+-- 用户表
+insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(9, "dakehu2", "dakehu2", 1, 10019);
+
+
+
+-- 插入权限
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(136, 9, 1);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(137, 9, 2);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(138, 9, 11);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(139, 9, 13);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(140, 9, 14);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(141, 9, 16);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(142, 9, 17);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(143, 9, 18);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(144, 9, 19);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(145, 9, 20);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(146, 9, 22);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(147, 9, 23);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(148, 9, 25);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(149, 9, 26);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 账号表
+insert into gm_account(id, gm_user_name, gm_password) values(10, "jiweihao", "29dbe8a36a1968d36b7e3eb1cff37391");
+
+-- 用户表
+insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(10, "jiweihao", "jiweihao", 1, 10019);
+
+
+
+-- 插入权限
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(150, 10, 1);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(151, 10, 2);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(152, 10, 11);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(153, 10, 13);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(154, 10, 14);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(155, 10, 16);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(156, 10, 17);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(157, 10, 18);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(158, 10, 19);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(159, 10, 20);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(160, 10, 22);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(161, 10, 23);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(162, 10, 25);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(163, 10, 26);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 账号表
+insert into gm_account(id, gm_user_name, gm_password) values(11, "zenghao", "a165ed173142f487aff06f6ea64cf893");
+
+-- 用户表
+insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(11, "zenghao", "zenghao", 1, 10019);
+
+
+
+-- 插入权限
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(164, 11, 1);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(165, 11, 2);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(166, 11, 11);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(167, 11, 13);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(168, 11, 14);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(169, 11, 16);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(170, 11, 17);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(171, 11, 18);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(172, 11, 19);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(173, 11, 20);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(174, 11, 22);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(175, 11, 23);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(176, 11, 25);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(177, 11, 26);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 账号表
+insert into gm_account(id, gm_user_name, gm_password) values(12, "huangshuoyuan", "d479f83becede0812d52ecffefbd6eff");
+
+-- 用户表
+insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(12, "huangshuoyuan", "huangshuoyuan", 1, 10019);
+
+
+
+-- 插入权限
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(178, 12, 1);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(179, 12, 2);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(180, 12, 11);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(181, 12, 13);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(182, 12, 14);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(183, 12, 16);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(184, 12, 17);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(185, 12, 18);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(186, 12, 19);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(187, 12, 20);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(188, 12, 22);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(189, 12, 23);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(190, 12, 25);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(191, 12, 26);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 账号表
+insert into gm_account(id, gm_user_name, gm_password) values(13, "tangzhihong", "d928e6bfa779a5ab5e58e2ffc73c046f");
+
+-- 用户表
+insert into gm_user(gm_user_id, gm_user_name, gm_user_realname, isused, game_type) values(13, "tangzhihong", "tangzhihong", 1, 10019);
+
+
+
+-- 插入权限
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(192, 13, 1);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(193, 13, 2);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(194, 13, 11);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(195, 13, 13);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(196, 13, 14);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(197, 13, 16);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(198, 13, 17);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(199, 13, 18);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(200, 13, 19);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(201, 13, 20);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(202, 13, 22);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(203, 13, 23);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(204, 13, 25);
+insert into gm_user_auth(id, gm_user_id, gm_auth_id) values(205, 13, 26);
+
+
